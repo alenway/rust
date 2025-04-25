cac0fa03f13d8d84b0967298f4ec066304cb1909
Narendra Samtani <alenway97@gmail.com>
2025-04-25 10:50:47 +0530

    basic datatypes in rust

---
+++ foundations/datatypes/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let x: i32 = -54;
+    let y: u64 = 4483;
+    println!("Signed integer: {}", x);
+    println!("unsigned integer: {}", y);
+    let pi: f64 = 3.14;
+    println!("value of pi: {}", pi);
+    let is_snowing: bool = true;
+    println!("Is it snowing, {}", is_snowing);
+    let letter: char = 'a';
+    println!("First letter of the alphabel: {}", letter);
+}
